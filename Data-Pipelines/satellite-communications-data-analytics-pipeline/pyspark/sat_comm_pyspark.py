c0006544254deede357353f3514d6035bbb1f781
PrathameshTanavade <tanavadep@gmail.com>
2023-06-26 22:07:49 -0400

    update -pyspark

--- Data-Pipelines/satellite-communications-data-analytics-pipeline/pyspark/sat_comm_pyspark.py
+++ Data-Pipelines/satellite-communications-data-analytics-pipeline/pyspark/sat_comm_pyspark.py
@@ -4,62 +4,47 @@ from dotenv import load_dotenv
 load_dotenv()
 MONGODB_URI=os.environ["MONGODB_URI"]
 MONGODB_DATABASE=os.environ["MONGODB_DATABASE"]
-MONGODB_COLLECTION=os.environ["MONGODB_COLLECTION"]
+MONGODB_RAW_COLLECTION=os.environ["MONGODB_RAW_COLLECTION"]
+MONGODB_PROCESSED_COLLECTION=os.environ["MONGODB_PROCESSED_COLLECTION"]
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, IntergerType, StringType, TimestampType
 from pyspark.sql.functions import *
 
 
 
-spark=SparkSession.builder\
-        .appName("Satellite Communication Data Pipeline")\
-        .config("spark.jars.packages","org.mongodb.spark:mongo-spark-connector:10.0.5")\
-        .getOrCreate()
-
-schema= StructType()\
-        .add("datetime", TimestampType())\
-        .add("remoteId", StringType())\
-        .add("beamId", IntegerType())\
-        .add("beamName", StringType())\
-        .add("satLong", IntegerType())\
-        .add("fwdModCodId", StringType())\
-        .add("fwdSNR", IntegerType())\
-        .add("packetsLost", IntegerType())\
-        .add("latitude", IntegerType())\
-        .add("longitude", IntegerType())\
-        .add("rxFreq", IntegerType())\
-        .add("txFreq", IntegerType())\
-        .add("fwdBitRate", IntegerType())\
-        
 
 
 
+def sat_comm(spark: SparkSession) -> None:
 
-lines=spark\
-        .readStream\
+    data=spark\
+        .read\
         .format("mongodb")\
-        .option("connection.uri",MONGODB_URI)
-        .option("database",MONGODB_DATABASE)
+        .option("connection.uri",MONGODB_URI)\
+        .option("database",MONGODB_DATABASE)\
         .option("collection",MONGODB_RAW_COLLECTION)\
-        .schema(schema)
-        .load()
-
-
-
-
-
-
-dataStreamWriter=transformation.writeStream\
+    	.option("spark.mongodb.change.stream.publish.full.document.only", "true")\
+        .load()\
+        .select('*').where("remoteId like 'C%'")\
+        .write\
         .format("mongodb")\
-        .option("checkpointlocation","/tmp/pyspark")\
-        .option("forceDeleteTempCheckpointLocation","true")\
+        .option("checkpointlocation","checkpoints")\
         .option("connection.uri", MONGODB_URI)\
         .option("database",MONGODB_DATABASE)\
         .option("collection",MONGODB_PROCESSED_COLLECTION)\
-        .trigger(continuous="20 second")\
-        .outputMode("append")\
-        .start()
+        .mode("append")\
+        .save()
 
-dataStreamWriter.awaitTermination()
+
+if __name__ == "__main__":
+    spark=SparkSession.builder\
+            .appName("Satellite Communication Data Pipeline")\
+            .config("spark.jars.packages","org.mongodb.spark:mongo-spark-connector:10.0.5")\
+            .getOrCreate()
+
+    sat_comm(spark)
+
+
+    spark.stop()
+
 
